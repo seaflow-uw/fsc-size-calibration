7a104de33fede53d426d41036da9b5582088b277
Francois Ribalet <ribalet@uw.edu>
2018-10-03 16:06:29 -0700

    add Mie theory

--- calibration.R
+++ calibration.R
@@ -2,81 +2,45 @@
 ### LINEAR REGRESSION ###
 #########################
 library(scales)
-path.to.git.repository <- "~/Documents/DATA/Codes/fsc-volume-calibration"
+path.to.git.repository <- "~/Documents/DATA/Codes/fsc-size-calibration"
 setwd(path.to.git.repository)
-culture <- read.csv("scatter_calibration.csv")
-culture <- culture[order(culture$norm.fsc),]
+.rainbow.cols <- colorRampPalette(c("#00007F", "blue", "#007FFF", "cyan", "#7FFF7F", "yellow","#FF7F00", "red", "#7F0000"))
 
-# convert diameter to volume, assuming spherical shape
+culture <- read.csv("scatter_calibration.csv")
+culture$norm.fsc <- culture$fsc / culture$fsc.beads
+culture$norm.fsc.sd <- culture$fsc.sd / culture$fsc.beads
 culture$volume <- 4/3 * pi * (culture$diameter/2)^3
 culture$volume.sd <- culture$volume * culture$diameter.sd/culture$diameter
 
+culture2 <- aggregate(culture, by=list(culture$species), FUN=mean)
+culture2 <- culture2[order(culture2$norm.fsc),]
 
-df <- t(read.table("meidata.txt"))
-df <- data.frame(cbind(diam=df[1:100,], scatter=df[101:200,]))
-write.csv(df, "meidata.csv", row.names=F, quote=F)
 mie <- read.csv("meidata.csv")
-
-par(mfrow=c(1,1))
-plot(mie$scatter/17000, mie$diam, log='xy',type='l', ylim=c(0.1,15), xlab="FSC",ylab="Diameter (µm)")
-f <- mie[which(mie$diam == 1),'scatter']
-with(culture, arrows(norm.fsc, diameter-culture$diameter.sd, norm.fsc, diameter + culture$diameter.sd,  code = 3, length=0, col='grey',lwd=2))
-with(culture, arrows(norm.fsc-norm.fsc.sd, diameter, norm.fsc+norm.fsc.sd, diameter,  code = 3, length=0,col='grey',lwd=2))
-points(culture$norm.fsc,culture$diameter,pch=16, col=2,cex=2)
-
-
-
-reg <- lm(diameter ~ norm.fsc, data=log(culture[1:8,c("diameter","norm.fsc")],10))
-reg2 <- lm(diameter ~ norm.fsc, data=log(culture[9:25,c("diameter","norm.fsc")],10))
+mie$scatter <- mie$scatter/14000 # to scale with empirical data
+mie$diam <- 1.6*mie$diam
+mie$volume <- 4/3 * pi * (mie$diam/2)^3
 
 png("Seaflow-diameter-scatter.png",width=12, height=12, unit='in', res=100)
 
-par(mfrow=c(1,1), pty='s',cex=1.4)
-plot(culture$norm.fsc, culture$diameter, log='xy', bg=alpha('grey',0.3), pch=21,ylab=substitute(paste("Cell diameter (",mu,"m)")), xlab="Normalized scatter (dimensionless)",cex=2, xaxt='n', yaxt='n')
-with(culture, arrows(norm.fsc, diameter- culture$diameter.sd, norm.fsc, diameter + culture$diameter.sd,  code = 3, length=0))
-with(culture, arrows(norm.fsc-norm.fsc.sd, diameter, norm.fsc+norm.fsc.sd, diameter,  code = 3, length=0))
-axis(1, at=c(0.05,0.1,0.5,1,5), labels=c(0.05,0.1,0.5,1,5))
-axis(2, at=c(0.1,1,2,5,10,20), labels=c(0.1,1,2,5,10,20))
-par(new=T)
-plot(log10(culture$norm.fsc), log10(culture$diameter), yaxt='n',xaxt='n',xlab=NA, ylab=NA,pch=NA, bty='n')
-lines(x=log10(culture$norm.fsc),predict(reg, newdata=data.frame(norm.fsc=log10(culture$norm.fsc)),interval='predict')[,"fit"], col='red3',lwd=2 )
-lines(x=log10(culture$norm.fsc),predict(reg, newdata=data.frame(norm.fsc=log10(culture$norm.fsc)),interval='predict')[,"upr"], col='grey',lwd=2 )
-lines(x=log10(culture$norm.fsc),predict(reg, newdata=data.frame(norm.fsc=log10(culture$norm.fsc)),interval='predict')[,"lwr"], col='grey',lwd=2 )
-legend("topleft", legend=bquote(paste("Diam_cyano=",.(round(10^reg$coefficients[1],3)),"(scatter"^{.(round(reg$coefficients[2],3))},")")), bty='n',cex=1.5, text.col='red3')
+  par(mfrow=c(1,1), pty='s',cex=1.4)
+  plot(culture2$norm.fsc, culture2$diameter, log='xy', pch=NA,ylab=substitute(paste("Cell diameter (",mu,"m)")), xlab="Normalized scatter (dimensionless)",cex=2, xaxt='n', yaxt='n', xlim=c(0.01,10), ylim=c(0.5,20))
+  with(culture2, arrows(norm.fsc, diameter-culture2$diameter.sd, norm.fsc, diameter + culture2$diameter.sd,  code = 3, length=0, col='grey',lwd=2))
+  with(culture2, arrows(norm.fsc-norm.fsc.sd, diameter, norm.fsc+norm.fsc.sd, diameter,  code = 3, length=0,col='grey',lwd=2))
+  axis(1, at=c(0.01,0.02,0.05,0.1,0.5,1,5), labels=c(0.01,0.02,0.05,0.1,0.5,1,5))
+  axis(2, at=c(0.1,0.2,0.5,1,2,5,10,20), labels=c(0.1,0.2,0.5,1,2,5,10,20), las=1)
+  lines(mie$scatter, mie$diam, col='red3',lwd=2)
+  points(culture2$norm.fsc, culture2$diameter, bg=alpha(.rainbow.cols(nrow(culture2)),0.5), pch=21,cex=2)
+  legend("topleft",legend=c(as.vector(culture2$Group.1),"Theoritical data"), pch=c(rep(21,nrow(culture2)),NA), lwd=c(rep(NA,nrow(culture2)),2), bty='n',
+            pt.bg=alpha(.rainbow.cols(nrow(culture2)),0.5), col=c(rep(1,nrow(culture2)),'red3'), text.font=c(rep(3,nrow(culture2)),1))
 
-lines(x=log10(culture$norm.fsc),predict(reg2, newdata=data.frame(norm.fsc=log10(culture$norm.fsc)),interval='predict')[,"fit"], col='seagreen3',lwd=2 )
-lines(x=log10(culture$norm.fsc),predict(reg2, newdata=data.frame(norm.fsc=log10(culture$norm.fsc)),interval='predict')[,"upr"], col='grey',lwd=2 )
-lines(x=log10(culture$norm.fsc),predict(reg2, newdata=data.frame(norm.fsc=log10(culture$norm.fsc)),interval='predict')[,"lwr"], col='grey',lwd=2 )
-legend(y=log10(10), x=log10(0.022), legend=bquote(paste("Diam_pico=",.(round(10^reg2$coefficients[1],3)),"(scatter"^{.(round(reg2$coefficients[2],3))},")")), bty='n',cex=1.5, text.col='seagreen3')
-
-dev.off()
-
-
-
-
-
-reg <- lm(volume ~ norm.fsc, data=log(culture[1:8,c("volume","norm.fsc")],10))
-reg2 <- lm(volume ~ norm.fsc, data=log(culture[9:25,c("volume","norm.fsc")],10))
-
-png("Seaflow-volume-scatter.png",width=12, height=12, unit='in', res=100)
-
-par(mfrow=c(1,1), pty='s',cex=1.4)
-plot(culture$norm.fsc, culture$volume, log='xy', bg=alpha('grey',0.3), pch=21,ylab=substitute(paste("Cell Volume (",mu,"m"^{3},")")), xlab="Normalized scatter (dimensionless)",cex=2, xaxt='n', yaxt='n')
-with(culture, arrows(norm.fsc, volume - culture$volume.sd, norm.fsc, volume + culture$volume.sd,  code = 3, length=0))
-with(culture, arrows(norm.fsc-norm.fsc.sd, volume, norm.fsc+norm.fsc.sd, volume,  code = 3, length=0))
-axis(1, at=c(0.05,0.1,0.5,1,5), labels=c(0.05,0.1,0.5,1,5))
-axis(2, at=c(1,5,10,50,100,500,1000), labels=c(1,5,10,50,100,500,1000))
-par(new=T)
-plot(log10(culture$norm.fsc), log10(culture$volume), yaxt='n',xaxt='n',xlab=NA, ylab=NA,pch=NA, bty='n')
-lines(x=log10(culture$norm.fsc),predict(reg, newdata=data.frame(norm.fsc=log10(culture$norm.fsc)),interval='predict')[,"fit"], col='red3',lwd=2 )
-lines(x=log10(culture$norm.fsc),predict(reg, newdata=data.frame(norm.fsc=log10(culture$norm.fsc)),interval='predict')[,"upr"], col='grey',lwd=2 )
-lines(x=log10(culture$norm.fsc),predict(reg, newdata=data.frame(norm.fsc=log10(culture$norm.fsc)),interval='predict')[,"lwr"], col='grey',lwd=2 )
-legend("topleft", legend=bquote(paste("Vol_cyano=",.(round(10^reg$coefficients[1],3)),"(scatter"^{.(round(reg$coefficients[2],3))},")")), bty='n',cex=1.5, text.col='red3')
-
-lines(x=log10(culture$norm.fsc),predict(reg2, newdata=data.frame(norm.fsc=log10(culture$norm.fsc)),interval='predict')[,"fit"], col='seagreen3',lwd=2 )
-lines(x=log10(culture$norm.fsc),predict(reg2, newdata=data.frame(norm.fsc=log10(culture$norm.fsc)),interval='predict')[,"upr"], col='grey',lwd=2 )
-lines(x=log10(culture$norm.fsc),predict(reg2, newdata=data.frame(norm.fsc=log10(culture$norm.fsc)),interval='predict')[,"lwr"], col='grey',lwd=2 )
-legend(y=log10(500), x=log10(0.022), legend=bquote(paste("Vol_pico=",.(round(10^reg2$coefficients[1],3)),"(scatter"^{.(round(reg2$coefficients[2],3))},")")), bty='n',cex=1.5, text.col='seagreen3')
-
+  # plot(culture2$norm.fsc, culture2$volume, log='xy', pch=NA,ylab=substitute(paste("Cell volume (",mu,"m3)")), xlab="Normalized scatter (dimensionless)",cex=2, xaxt='n', yaxt='n', xlim=c(0.01,10))
+  # with(culture2, arrows(norm.fsc, volume-culture2$volume.sd, norm.fsc, volume + culture2$volume.sd,  code = 3, length=0, col='grey',lwd=2))
+  # with(culture2, arrows(norm.fsc-norm.fsc.sd, volume, norm.fsc+norm.fsc.sd, volume,  code = 3, length=0,col='grey',lwd=2))
+  # axis(1, at=c(0.01,0.02,0.05,0.1,0.5,1,5), labels=c(0.01,0.02,0.05,0.1,0.5,1,5))
+  # axis(2, at=c(0.1,0.2,0.5,1,2,5,10,20,50,100,200,500,1000), labels=c(0.1,0.2,0.5,1,2,5,10,20,50,100,200,500,1000), las=1)
+  # lines(mie$scatter, mie$volume, col='red3',lwd=2)
+  # points(culture2$norm.fsc, culture2$volume, bg=alpha(.rainbow.cols(nrow(culture2)),0.5), pch=21,cex=2)
+  # legend("topleft",legend=c(as.vector(culture2$Group.1),"Theoritical data"), pch=c(rep(21,nrow(culture2)),NA), lwd=c(rep(NA,nrow(culture2)),2), bty='n',
+  #           pt.bg=alpha(.rainbow.cols(nrow(culture2)),0.5), col=c(rep(1,nrow(culture2)),'red3'), text.font=c(rep(3,nrow(culture2)),1))
 
 dev.off()
