b7111c48bdc2fdcad2baa55c44b0dde673b209ee
Francois Ribalet <ribalet@uw.edu>
2018-10-04 15:02:45 -0700

    clean up calibrated Mie file

--- calibration.R
+++ calibration.R
@@ -15,7 +15,7 @@ culture$volume.sd <- culture$volume * culture$diameter.sd/culture$diameter
 culture2 <- aggregate(culture, by=list(culture$species), FUN=mean)
 culture2 <- culture2[order(culture2$norm.fsc),]
 
-mie <- read.csv("mie_calibrated.csv")
+mie <- read.csv("calibrated-mie.csv")
 inst <- 751
 
 png(paste0(inst,"-Size-scatter.png"),width=12, height=12, unit='in', res=100)
@@ -26,7 +26,7 @@ png(paste0(inst,"-Size-scatter.png"),width=12, height=12, unit='in', res=100)
   with(culture2, arrows(norm.fsc-norm.fsc.sd, diameter, norm.fsc+norm.fsc.sd, diameter,  code = 3, length=0,col='grey',lwd=2))
   axis(1, at=c(0.01,0.02,0.05,0.1,0.2,0.5,1,2,5))
   axis(2, at=c(0.1,0.2,0.5,1,2,5,10,20),las=1)
-  lines(mie[,paste0("scatter_",inst)], mie[,paste0("diam_",inst)], col='red3',lwd=2)
+  lines(mie[,paste0("scatter_",inst)], mie$diam, col='red3',lwd=2)
   points(culture2$norm.fsc, culture2$diameter, bg=alpha(.rainbow.cols(nrow(culture2)),0.5), pch=21,cex=2)
   legend("topleft",legend=c(as.vector(culture2$Group.1),"Theoritical data"), pch=c(rep(21,nrow(culture2)),NA), lwd=c(rep(NA,nrow(culture2)),2), bty='n',
             pt.bg=alpha(.rainbow.cols(nrow(culture2)),0.5), col=c(rep(1,nrow(culture2)),'red3'), text.font=c(rep(3,nrow(culture2)),1))
